afb5543d319b838d397b880392c01402d7a23e49
ievgen-iukhymovych <Ievgen_Iukhymovych@epam.com>
2018-03-28 11:06:28 +0300

    Add project files.

---
+++ CommonUtils/Bencode/BencodeParser.cs
@@ -0,0 +1,13 @@
+﻿namespace CommonUtils.Bencode
+{
+    public class BencodeParser
+    {
+        public void Parse(string beString)
+        {
+            
+        }
+
+
+        public void ParseFile(string path) { }
+    }
+}
---
+++ TorrPlayground/TorrentFile.cs
@@ -0,0 +1,7 @@
+﻿namespace TorrPlayground
+{
+    public class TorrentFile
+    {
+        
+    }
+}
